5584dfeadd8cf74916e2b0cc1b83511d67744af1
GAMING\equat <hello@marcodeambrosi.me>
2017-11-24 17:02:06 +0100

    Working Scriptable Object for Enemy Spawner!

--- Assets/Project Bastion/Scripts/Managers/EnemySpawnManager.cs
+++ Assets/Project Bastion/Scripts/Managers/EnemySpawnManager.cs
@@ -3,26 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class EnemySpawnManager: MonoBehaviour  {
+public abstract class EnemySpawnManager: ScriptableObject  
+{
 
-
-	public Transform enemySpawnPoint; 				// List to the enemy Spawn Position.
-	public EnemyManager enemyManager;				// Reference to the Enemy Manager.
-	public GameObject enemyPrefab;					// Reference to the Enemy prefab.
-	public float timebetweenSpawns = 5f;			// Reference to the time between a spawn and another. 
-
-
-	// Keep Spawning monsters
-	public void StartSpawningEnemies()
-	{
-		Debug.Log ("Starting coroutine for enemy spawn");
-		SpawnMonster (enemyPrefab, enemySpawnPoint);
-	}
-		
 	// Spawn Enemy with selected prefab and position.
-	public void SpawnMonster(GameObject prefab, Transform position)
-	{
-		enemyManager.enemyInstance = GameObject.Instantiate(enemyPrefab, position.position, position.rotation) as GameObject;
-	}
-		
+	public abstract void SpawnMonster(GameObject prefab, Transform position);
+	public abstract void StartSpawningEnemies ();
 }
---
+++ Assets/Project Bastion/Scripts/Managers/SpawnManagers/SimpleEnemySpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu (menuName= "SpawnManager/SimpleEnemySpawner" )]
+public class SimpleEnemySpawnManager : EnemySpawnManager {
+
+
+	public Transform enemySpawnPoint; 				// List to the enemy Spawn Position.
+	public EnemyManager enemyManager;				// Reference to the Enemy Manager.
+	public GameObject enemyPrefab;					// Reference to the Enemy prefab.
+	public float timebetweenSpawns = 5f;			// Reference to the time between a spawn and another. 
+
+
+	// Keep Spawning monsters
+	public override void StartSpawningEnemies()
+	{
+		Debug.Log ("Starting coroutine for enemy spawn");
+		SpawnMonster (enemyPrefab, enemySpawnPoint);
+	}
+		
+	// Spawn Enemy with selected prefab and position.
+	public override void SpawnMonster(GameObject prefab, Transform position)
+	{
+		enemyManager.enemyInstance = GameObject.Instantiate(enemyPrefab, position.position, position.rotation) as GameObject;
+	}
+		
+}
