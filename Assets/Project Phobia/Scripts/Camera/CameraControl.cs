e3f30f92626220cb935fdda2fe791abeb99d2e75
GAMING\equat <hello@marcodeambrosi.me>
2017-12-05 11:34:50 +0100

    Changed Camera script so that the camera now follows player view. Added script to calculate velocity of a Kinetic object.

--- Assets/Project Phobia/Scripts/Camera/CameraControl.cs
+++ Assets/Project Phobia/Scripts/Camera/CameraControl.cs
@@ -6,145 +6,151 @@
 
 public class CameraControl : MonoBehaviour
 {
-	public float m_DampTime = 0.2f;				// Approximate time for the camera to refocus.
-	public float m_ScreenEdgeBuffer = 4f;		// Space between the top/bottom most target and the screen edge.
-	public float m_MinSize = 6.5f;				// The smallest orthographic size the camera can be.
-	public Transform[] m_Targets;				// All the targets the camera needs to encompass.
-	public float velocityAmplification = 0.8f;	// Set how much the velocity of the object influence the movement of the camera ahead.
+    public float m_DampTime = 0.2f;             // Approximate time for the camera to refocus.
+    public float m_ScreenEdgeBuffer = 4f;       // Space between the top/bottom most target and the screen edge.
+    public float m_MinSize = 6.5f;              // The smallest orthographic size the camera can be.
+    public Transform[] m_Targets;               // All the targets the camera needs to encompass.
+    public float velocityAmplification = 0.8f;  // Set how much the velocity of the object influence the movement of the camera ahead.
 
-	private Camera m_Camera;					// Used for referencing the camera.
-	private float m_ZoomSpeed;					// Reference speed for the smooth damping of the orthographic size.
-	private Vector3 m_MoveVelocity;				// Reference velocity for the smooth damping of the position.
-	private Vector3 m_DesiredPosition;			// The position the camera is moving towards.
+    private Camera m_Camera;                    // Used for referencing the camera.
+    private float m_ZoomSpeed;                  // Reference speed for the smooth damping of the orthographic size.
+    private Vector3 m_MoveVelocity;             // Reference velocity for the smooth damping of the position.
+    private Vector3 m_DesiredPosition;          // The position the camera is moving towards.
 
-	private void Awake ()
-	{
-		m_Camera = GetComponentInChildren<Camera> ();
+    private void Awake()
+    {
+        m_Camera = GetComponentInChildren<Camera>();
+		m_MoveVelocity = GetComponent<CalculateVelocity> ().currVel;
 
-	}
-
-
-	private void FixedUpdate ()
-	{
-		// Move the camera towards a desired position.
-		Move ();
-
-		// Change the size of the camera based.
-		Zoom ();
-	}
+    }
 
 
-	private void Move ()
-	{
-		// If there are more than 1 targets, find the average position of the targets.
-		if (m_Targets.Length == 1) {	
-			FindPositionBasedOnVelocity ();
-		} else {
-			FindAveragePosition ();
-		}
+    private void FixedUpdate()
+    {
+        // Move the camera towards a desired position.
+        Move();
 
-		// Smoothly transition to that position.
-		transform.position = Vector3.SmoothDamp (transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
-	}
-
-	//this class is to move the camera ahead of the player based on the velocity of the player.
-	//NEEDS SOME REFACTORING AS IT'S A ONE TRICK METHOD
-	private void FindPositionBasedOnVelocity ()
-	{
-
-		//NEED TO ADD CHECK IF TARGET IS A RIGIDBODY, IF NOT PROVIDE A VECTOR FOR THE VELOCITY
-		Vector3 m_TargetVelocity = m_Targets[0].GetComponent<PlayerMovementKeyboardAndMouse>().m_MovementVelocityValue;
-
-		// Add the velocity vector multiplied an amplification factor to the vector of the target position. The camera will follow that vector point.
-		Vector3 targetCamPos = m_Targets[0].position + m_TargetVelocity*velocityAmplification;
-
-		// The desired position is the average position;
-		m_DesiredPosition = targetCamPos;
-	}
+        // Change the size of the camera based.
+        Zoom();
+    }
 
 
-	private void FindAveragePosition ()
-	{
-		Vector3 averagePos = new Vector3 ();
-		int numTargets = 0;
+    private void Move()
+    {
+        // If there are more than 1 targets, find the average position of the targets.
+        if (m_Targets.Length == 1)
+        {
+            FindPositionBasedOnView();
+        }
+        else
+        {
+            FindAveragePosition();
+        }
 
-		// Go through all the targets and add their positions together.
-		for (int i = 0; i < m_Targets.Length; i++) {
-			// If the target isn't active, go on to the next one.
-			if (!m_Targets [i].gameObject.activeSelf)
-				continue;
+        // Smoothly transition to that position.
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
 
-			// Add to the average and increment the number of targets in the average.
-			averagePos += m_Targets [i].position;
-			numTargets++;
-		}
+    //this class is to move the camera ahead of the player based on the front facing of the player.
+    //NEEDS SOME REFACTORING AS IT'S A ONE TRICK METHOD
+    private void FindPositionBasedOnView()
+    {
 
-		// If there are targets divide the sum of the positions by the number of them to find the average.
-		if (numTargets > 0)
-			averagePos /= numTargets;
+        //NEED TO ADD CHECK IF TARGET IS A RIGIDBODY, IF NOT PROVIDE A VECTOR FOR THE VELOCITY
+		Vector3 m_TargetVelocity = m_Targets[0].GetComponent<PlayerMovementKeyboardAndMouse>().transform.forward;
 
-		// Keep the same y value.
-		averagePos.y = transform.position.y;
+        // Add the velocity vector multiplied an amplification factor to the vector of the target position. The camera will follow that vector point.
+        Vector3 targetCamPos = m_Targets[0].position + m_TargetVelocity * velocityAmplification;
 
-		// The desired position is the average position;
-		m_DesiredPosition = averagePos;
-	}
-
-
-	private void Zoom ()
-	{
-		// Find the required size based on the desired position and smoothly transition to that size.
-		float requiredSize = FindRequiredSize ();
-		m_Camera.orthographicSize = Mathf.SmoothDamp (m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
-	}
+        // The desired position is the average position;
+        m_DesiredPosition = targetCamPos;
+    }
 
 
-	private float FindRequiredSize ()
-	{
-		// Find the position the camera rig is moving towards in its local space.
-		Vector3 desiredLocalPos = transform.InverseTransformPoint (m_DesiredPosition);
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
 
-		// Start the camera's size calculation at zero.
-		float size = 0f;
+        // Go through all the targets and add their positions together.
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            // If the target isn't active, go on to the next one.
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
 
-		// Go through all the targets...
-		for (int i = 0; i < m_Targets.Length; i++) {
-			// ... and if they aren't active continue on to the next target.
-			if (!m_Targets [i].gameObject.activeSelf)
-				continue;
+            // Add to the average and increment the number of targets in the average.
+            averagePos += m_Targets[i].position;
+            numTargets++;
+        }
 
-			// Otherwise, find the position of the target in the camera's local space.
-			Vector3 targetLocalPos = transform.InverseTransformPoint (m_Targets [i].position);
+        // If there are targets divide the sum of the positions by the number of them to find the average.
+        if (numTargets > 0)
+            averagePos /= numTargets;
 
-			// Find the position of the target from the desired position of the camera's local space.
-			Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+        // Keep the same y value.
+        averagePos.y = transform.position.y;
 
-			// Choose the largest out of the current size and the distance of the tank 'up' or 'down' from the camera.
-			size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
-
-			// Choose the largest out of the current size and the calculated size based on the tank being to the left or right of the camera.
-			size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
-		}
-
-		// Add the edge buffer to the size.
-		size += m_ScreenEdgeBuffer;
-
-		// Make sure the camera's size isn't below the minimum.
-		size = Mathf.Max (size, m_MinSize);
-
-		return size;
-	}
+        // The desired position is the average position;
+        m_DesiredPosition = averagePos;
+    }
 
 
-	public void SetStartPositionAndSize ()
-	{
-		// Find the desired position.
-		FindAveragePosition ();
+    private void Zoom()
+    {
+        // Find the required size based on the desired position and smoothly transition to that size.
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+    }
 
-		// Set the camera's position to the desired position without damping.
-		transform.position = m_DesiredPosition;
 
-		// Find and set the required size of the camera.
-		m_Camera.orthographicSize = FindRequiredSize ();
-	}
+    private float FindRequiredSize()
+    {
+        // Find the position the camera rig is moving towards in its local space.
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        // Start the camera's size calculation at zero.
+        float size = 0f;
+
+        // Go through all the targets...
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            // ... and if they aren't active continue on to the next target.
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            // Otherwise, find the position of the target in the camera's local space.
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+
+            // Find the position of the target from the desired position of the camera's local space.
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+            // Choose the largest out of the current size and the distance of the tank 'up' or 'down' from the camera.
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
+
+            // Choose the largest out of the current size and the calculated size based on the tank being to the left or right of the camera.
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
+        }
+
+        // Add the edge buffer to the size.
+        size += m_ScreenEdgeBuffer;
+
+        // Make sure the camera's size isn't below the minimum.
+        size = Mathf.Max(size, m_MinSize);
+
+        return size;
+    }
+
+
+    public void SetStartPositionAndSize()
+    {
+        // Find the desired position.
+        FindAveragePosition();
+
+        // Set the camera's position to the desired position without damping.
+        transform.position = m_DesiredPosition;
+
+        // Find and set the required size of the camera.
+        m_Camera.orthographicSize = FindRequiredSize();
+    }
 }
---
+++ Assets/Project Phobia/Scripts/Player/CalculateVelocity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This Script will calculate the velocity of the Kinetic Object.
+
+public class CalculateVelocity : MonoBehaviour {
+
+	[HideInInspector]
+	public Vector3 currVel;					// Current velocity of the Object.
+
+	// Use this for initialization
+	void Awake () {
+		StartCoroutine (CalcVelocity ());
+	}
+	
+	IEnumerator CalcVelocity()
+	{
+		while (Application.isPlaying) {
+			// Position at the frame start.
+			Vector3 prevPos = (transform.position) ;
+
+			// Wait till it the end of the frame.
+			yield return new WaitForEndOfFrame ();
+
+			// Calculate velocity
+
+			currVel = ((prevPos - transform.position) / Time.deltaTime);
+	}
+}
+}
