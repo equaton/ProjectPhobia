df78af470f28fdf5feaf5b4169ea90fb346ca1e7
GAMING\equat <hello@marcodeambrosi.me>
2018-03-15 23:55:47 +0100

    Now Enemy stops following player if dark after some seconds

---
+++ Assets/Project Phobia/Scripts/AI/Decisions/LightChasingDecision.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName= "EnemyAI/Decisions/LightChasingDecision")]
+public class LightChasingDecision : EnemyDecision {
+
+	private float startLightningVeloocity = 0.1f;
+	private float startLightningThreshold = 1f;
+	public float damptime = 0.5f;
+
+	public override bool Decide (StateController controller)
+	{
+		bool targetVisible = SeeLight (controller);
+		return targetVisible;
+
+	}
+	//check if the objecty can see another with the Spherecast
+	private bool SeeLight (StateController controller)
+	{
+		//Check if the enemy is hit by the Light Aura - it is active around the player when the torchlight is on
+		Collider[] lightCollider = Physics.OverlapSphere (controller.gameObject.transform.position, controller.enemyStats.hearingSphereRange, 1 << 13);
+
+		//If it is hit
+		if (lightCollider.Length != 0) {
+			startLightningThreshold = Mathf.SmoothDamp (startLightningThreshold, 1.1f, ref startLightningVeloocity, damptime);
+			Debug.Log (startLightningThreshold);
+			if (startLightningThreshold > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			startLightningThreshold = Mathf.SmoothDamp (startLightningThreshold, 0f, ref startLightningVeloocity, damptime);
+			Debug.Log (startLightningThreshold);
+
+			if (startLightningThreshold < 0.01) {
+				return false;
+			} else {
+				return true;
+			}
+		
+	
+		}
+			
+	}
+}
